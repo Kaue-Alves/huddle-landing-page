45fe622fe0aa808351dbb19815223c797967e5f8
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-12-29 10:12:57 -0300

    organiza a configuração de cores no tema do Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-theme: {
-    extend: {},
-    colors: {
-        Pink: 'hsl(322, 100%, 66%)',
-        VerPaleCyan: 'hsl(193, 100%, 96%)',
-        VeryDarkCyan: 'hsl(192, 100%, 9%)',
-        GrayishBlue: 'hsl(208, 11%, 55%)',
+    theme: {
+        extend: {
+            colors: {
+                Pink: "hsl(322, 100%, 66%)",
+                VerPaleCyan: "hsl(193, 100%, 96%)",
+                VeryDarkCyan: "hsl(192, 100%, 9%)",
+                GrayishBlue: "hsl(208, 11%, 55%)",
+            },
+        },
     },
-},
     plugins: [],
 };
