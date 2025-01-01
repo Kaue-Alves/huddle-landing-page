ce125c308ff968a7f085a7b822432149f84d6424
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2025-01-01 18:17:35 -0300

    ajusta responsividade do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,11 @@ import Logo from "../assets/images/logo.png";
 
 export default function Header() {
   return (
-    <header className="bg-transparent w-full flex justify-between items-center px-5 lg:px-12 py-8">
-          <img src={Logo} className="w-auto h-6 sm:h-full"/>
-          <button className="text-black font-semibold bg-white rounded-full py-2 px-6 lg:py-3 lg:px-14 shadow-lg text-sm font-poppins">
+    <header className="bg-transparent w-full flex justify-between items-center px-5 md:px-12 py-8">
+          <img src={Logo} className="w-auto h-6 md:h-full"/>
+          <button className="text-black font-semibold bg-white rounded-full py-2 px-6 md:py-3 md:px-14 shadow-lg text-sm font-poppins">
               Try It Free
           </button>
     </header>
   )
 }
-
-104
